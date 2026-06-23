42c03fad741e50e99f1c436511adb46e87dcb755
mehedi <mehedi25122003@gmail.com>
2026-06-23 23:16:26 +0600

    err and err

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import express,{ Application, Request, Response } from "express";
 import config from "./config";
+import httpsStatus from "http-status";
+import { prisma } from "./lib/prisma";
+import bcrypt from "bcryptjs";
+import { profile } from "node:console";
 
 const app:Application = express();
 
@@ -17,5 +21,46 @@ app.use(cookieParser())
 app.get('/',(req:Request,res:Response)=>{
     res.send("Hello world!!");
 })
+
+app.post('/api/users/register',async (req:Request,res:Response)=>{
+const {name,email,password,profilePhoto} = req.body;
+const isUserExist = await prisma.user.findUnique({
+    where:{email}
+})
+if(isUserExist){
+    throw new Error("User already exists")
+}
+const hashedpassword = await bcrypt.hash(password,Number(config.bcrypt_salt_rounds))
+
+const createdUser = await prisma.user.create({
+    data:{
+        name,
+        email,
+        password:hashedpassword
+    }
+});
+await prisma.profile.create({
+    data:{
+        userId: createdUser.id,
+        profilePhoto
+    }
+})
+const user = await prisma.user.findUnique({
+    where:{
+        id: createdUser.id,
+        email:createdUser.email||email
+    },
+    include:{
+        profile: true
+    }
+}
+)
+
+res.status(httpsStatus.CREATED).json({meesage:"founda",data:{
+    user
+}})
+
+})
+
 
 export default app;
